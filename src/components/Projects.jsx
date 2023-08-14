4a6a864d9cfa72ec49b6bd74c5f44d085fe41222
Joshua Garvey <j.garvey514@yahoo.com>
2023-08-14 02:56:34 -0400

    overlay

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,8 +17,10 @@ function Projects() {
 	return (
 		<div ref={sliderRef} className="keen-slider h-5/6  p-4 flex bg-blue-300 border-2 border-red-800 ">
 			<div className="keen-slider__slide number-slide3 flex justify-center items-center">
-				<div className=" bg-[url('./images/square.png')] h-6/6 bg-cover rounded-xl border-2 border-black flex aspect-square  object-scale-down bg-center">
-					<h1 className="">Immersive 3d portfolio</h1>
+				<div className=" bg-[url('./images/square.png')] h-6/6 bg-cover rounded-xl border-2 border-black flex aspect-square  object-scale-down bg-center items-end">
+					<div class="w-full h-1/6 flex  items-center justify-center bottom-0 backdrop-brightness-50">
+						<span class="text-white text-lg text-bold   ">Immersive 3d Portfolio</span>
+					</div>
 				</div>
 			</div>
 
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/https://github.com/Jgar514/mobileUI.git",
+	base: "/mobileUI/",
 	plugins: [react()],
 });
